01fe9b422ef2eda5f7a069ea47aad47c681128e5
sanjaychoppadandi556-coder <sanjaychoppadandi556@gmail.com>
2026-07-05 02:47:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const noBtn = document.getElementById('noBtn');
-const yesBtn = document.getElementById('yesBtn');
-const message = document.getElementById('message');
-noBtn.addEventListener('mouseover', () => {
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
   const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
   const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
-  noBtn.style.left = '${x}px';
-  noBtn.stle.top = '${y}px';
+
+  noBtn.style.position = "fixed";
+  noBtn.style.left = `${x}px`;
+  noBtn.style.top = `${y}px`;
 });
-yesBtn.addEventListener('click', () => {
-  message.textContent = 'Yay! You said Yes! ';
-  message.style.display = 'block';
+
+yesBtn.addEventListener("click", () => {
+  message.textContent = "Yay! You said Yes! 💖";
+  message.style.display = "block";
 });
